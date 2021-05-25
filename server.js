77cdac8faca86efa555a6ec55c8c16f0d8acdf01
Sstark97 <aitorscinfo@gmail.com>
2021-05-25 12:50:34 +0100

    0Auth configuration

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ import passport from "passport";
 import cors from "cors";
 import router from './api/users/users.controller'
 
-mongoose.connect('mongodb://localhost/AllIn');
+mongoose.connect('mongodb://localhost/AllIn', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const app = express();
 const PORT = 3000;
 
 //MiddleWares
+app.use(passport.initialize());
+app.use(morgan('dev'))
 app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+//Se puede configurar más adelante
+app.use(cors())
 
 
 app.listen(PORT,() => {
