59c0ab2123079516fbb7d3c71b6e3f945838bfa0
Sstark97 <aitorscinfo@gmail.com>
2021-05-25 13:07:41 +0100

    Configure passport-google

---
+++ api/googleAuth/googleAuth.js
@@ -0,0 +1,61 @@
+import passport from "passport";
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+const User = require('../users/users.model').default;
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
+// Estrategia para Registrarse
+
+passport.use("sign-in-google",new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:4200/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const user = await User.findById(profile.id); // si el usuario no existe 
+                                                    //lo creamos
+      if (user) {
+        done(null, false);
+      } else {
+        let newUser = new User();
+            newUser._id = profile.id
+            newUser.nombre = profile.displayName
+            newUser.avatar = profile.photos[0].value
+           await newUser.save() //guardamos en la base de datos
+        done(null, profile); //guardamos en la base de datos
+      }
+    }
+  )
+);
+
+
+// Estrategia para Iniciar Sesion
+
+
+passport.use("sign-up-google",new GoogleStrategy(
+  {
+    clientID:"sjsjsjsjssswww",
+    clientSecret: "sjsjsjsjsjsj",
+    callbackURL: "http://localhost:4000/auth/google/signup",
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    const user = await User.findById(profile.id);// si existe en la base de datos
+                                                 //  puede iniciar sesion
+    if (user) {
+      done(null, user)
+    } else {
+      done(null, false)
+    }
+    
+  }
+)
+);
