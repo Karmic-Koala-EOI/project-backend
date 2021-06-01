8101715b3967f1371b439fde41000e359845edbc
Sstark97 <aitorscinfo@gmail.com>
2021-06-01 12:53:43 +0100

    First_Version_Social_Media

--- api/social_media/social_media.model.js
+++ api/social_media/social_media.model.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose');
 const model = mongoose.model;
 const Schema = mongoose.Schema;
+
+
+const socialSchema = new Schema ({
+    
+    social_media:{
+        type: String,
+        enum: ['twitter','instagram']
+    }
+})
+
+module.exports = model('socials', userSchema);
