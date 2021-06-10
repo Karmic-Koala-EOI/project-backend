fa5af70b7999bb008b36755606242e38c3fbb075
Sstark97 <aitorscinfo@gmail.com>
2021-06-10 17:04:42 +0100

    Vercel again...

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const User = require('./api/users/users.model');
 app.use(passport.initialize());
 app.use(morgan('dev'))
 app.use(express.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
+app.use(express.urlencoded({extended:false}))
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true}));
 app.use(cookieParser());
 
